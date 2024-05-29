6ec38399049798f49e00ff018e3b26a72d56812f
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-29 17:18:43 +0600

    User Verify, login and logout

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,28 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel"
+import {NextRequest,NextResponse} from "next/server"
+
+
+connect()
+
+export async function POST(request:NextRequest){
+    try {
+        const response = NextResponse.json({
+            message:"Logout User Successfully",
+            success: true 
+        })
+
+        response.cookies.set("token","",{
+            httpOnly:true,
+            expires: new Date(0)
+        })
+
+        return response
+        
+    } catch (error:any) {
+        return NextResponse.json({
+            error:error.message
+        },{status:500})
+        
+    }
+}
