abb9ab3ee20f37daa4256c6513bffcdb37cc4dab
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-30 13:00:22 +0600

    testing backend and all bugs fixed

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -5,7 +5,7 @@ import {NextRequest,NextResponse} from "next/server"
 
 connect()
 
-export async function POST(request:NextRequest){
+export async function GET(request:NextRequest){
     try {
         const response = NextResponse.json({
             message:"Logout User Successfully",
