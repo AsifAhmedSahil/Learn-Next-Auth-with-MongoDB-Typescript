7c823f934ec785307e74b6c92fa080bfb0f3cda3
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-30 12:38:43 +0600

    bug fixed - user verified successfully

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -9,9 +9,17 @@ export const sendEmail = async ({email,emailType,userId}:any) =>{
 
 
         if(emailType === "VERIFY"){
-            await User.findByIdAndUpdate(userId,{verifyToken:hashedToken, verifyTokenExpiry:Date.now() + 3600000})
+            await User.findByIdAndUpdate(userId,{
+              $set:{
+                verifyToken:hashedToken, verifyTokenExpiry:Date.now() + 3600000
+              }
+            })
         } else if(emailType === "RESET"){
-            await User.findByIdAndUpdate(userId,{forgotPasswordToken:hashedToken, forgotPasswordTokenExpiry:Date.now() + 3600000})
+            await User.findByIdAndUpdate(userId,{
+              $set:{
+                forgotPasswordToken:hashedToken, forgotPasswordTokenExpiry:Date.now() + 3600000
+              }
+            })
         }
 
         var transport = nodemailer.createTransport({
