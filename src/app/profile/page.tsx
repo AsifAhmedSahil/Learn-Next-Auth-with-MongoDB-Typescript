dddfeb2d5c900788fbcfc44b1c8de391c7c17046
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-31 17:55:01 +0600

    profile page setup

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,60 @@
-import React from 'react'
+/* eslint-disable react-hooks/rules-of-hooks */
+"use client"
+import axios from 'axios'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
+import toast from 'react-hot-toast'
 
-const page = () => {
+export default function profilePage() {
+  const router = useRouter()
+
+  const [data,setData] = useState("")
+
+  const getUserData = async() => {
+    try {
+      const res = await axios.post("api/users/me")
+      console.log(res.data.data._id)
+      setData(res.data.data._id)
+    } catch (error:any) {
+      console.log(error.message)
+      
+    }
+  }
+
+  const logout = async() =>{
+    try {
+      await axios.get("api/users/logout")
+      toast.success("logout Successfully")
+      router.push("/login")
+      
+    } catch (error:any) {
+      console.log(error.message)
+      toast.error(error.message)
+      
+    }
+  }
   return (
-    <div>
-      profile
+    <div className='flex flex-col justify-center items-center min-h-screen py-2'>
+      <h1 className='text-3xl'>Profile Page</h1>
+      <hr />
+
+      <h2>
+        {
+          data === "nothing" ? "Nothing" : <Link href={`/profile/${data}`}>{data}</Link>
+        }
+      </h2>
+
+
+      <button className='bg-blue-500 hover:bg-blue-700 text-white rounded px-4 py-2 mt-4' onClick={logout}>
+        logout
+      </button>
+      <button className='bg-green-500 hover:bg-green-700 text-white rounded px-4 py-2 mt-4' onClick={getUserData}>
+        Get user data
+      </button>
+      
     </div>
   )
 }
 
-export default page
+
